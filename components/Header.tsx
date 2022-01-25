402bacdf399a3a266db54d29fcb63b8f55345ab8
Tuhar Shrivastav <ts3657@gmail.com>
2022-01-25 14:42:43 +0530

    finsh the build

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,33 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="mx-auto flex max-w-7xl justify-between p-5 ">
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
         <Link href="/">
           <img
-            className="w-44 cursor-pointer object-contain"
+            className="w-44 object-contain cursor-pointer"
             src="https://links.papareact.com/yvf"
-            alt="logo"
+            alt=""
           />
         </Link>
-        <div className=" hidden items-center space-x-5 md:inline-flex ">
+        <div className="hidden md:inline-flex items-center space-x-5">
           <h3>About</h3>
-          <h3>Content</h3>
-          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+          <h3>Contact</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
             Follow
           </h3>
         </div>
       </div>
 
       <div className="flex items-center space-x-5 text-green-600">
-        <h3>sign in</h3>
-        <h3 className="cursor-pointer rounded-full border border-green-600 px-4 py-1">
+        <h3>Sign In</h3>
+        <h3 className="border border-green-600 px-4 py-1 rounded-full">
           Get Started
         </h3>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,92 @@
-import Head from 'next/head'
-import Header from '../components/Header'
+import Head from "next/head";
+import Link from "next/link";
+import Header from "../components/Header";
+import { sanityClient, urlFor } from "../Sanity";
+import { Post } from "../typings";
 
-export default function Home() {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({ posts }: Props) {
   return (
-    <div className="max-v-7xl mx-auto">
+    <div className="">
       <Head>
-        <title>Medium-Clone</title>
+        <title>Medium blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-
-      {/* main section start */}
-      <div className="flex items-center justify-between border-y border-black bg-yellow-400 p-10 lg:p-0">
-        <div className="space-y-5 px-10">
-          <h1 className="max-w-xl font-serif text-6xl">
+      {/* Banner */}
+      <div className="flex justify-between items-center bg-yellow-300 border-y border-black py-10 lg:py-0 max-w-7xl mx-auto">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
             <span className="underline decoration-black decoration-4">
               Medium
-            </span>{' '}
-            is a place to write, read, and content
+            </span>{" "}
+            is a place to write, read, and connect
           </h1>
           <h2>
-            it"s easy and free to post your thinking on any topic and connect
-            with millions of readers
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
           </h2>
         </div>
         <img
-          className="hidden h-32 md:inline-flex lg:h-full"
+          className="hidden md:inline-flex h-32 lg:h-full"
           src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
           alt="logo"
         />
       </div>
-      {/* main section end */}
+      {/* Posts */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6 max-w-7xl mx-auto">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="border rounded-lg group cursor-pointer overflow-hidden">
+              <img
+                className="rounded-t-lg h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <div>
+                  <img
+                    className="h-12 w-12 rounded-full object-cover"
+                    src={urlFor(post.author.image).url()!}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"] {
+  _id,
+  title,
+  author-> {
+  name,
+  image
+},
+description,
+mainImage,
+slug
+}`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
